ab11bdbbf7cfdec2122c63548347005f1082a4f9
edith007 <siddharthasthana31@gmail.com>
2020-07-19 12:01:06 +0530

    Done with Profile Page

--- IMDB/urls.py
+++ IMDB/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
@@ -24,6 +24,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'),
     path('profile/', user_views.profile, name='profile'),
-    path('profile/<str:user>/', user_views.profile, name='profile'),
+    path('profile/<str:username>/', user_views.profile, name='profile'),
     path('', include('movie.urls')),
 ]
